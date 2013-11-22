798c55383caa5672464c834a56560e4b98fb6aa3
Samuel Rats <srats@genymobile.com>
2013-11-22 12:01:28 +0100

    [US202] Add else clause

--- core/main.mk
+++ core/main.mk
@@ -293,8 +293,10 @@ user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
-  # Target is secure in user builds.
-  ifneq ($(user_variant),userdebug)
+  # Target is secure in user builds, not in userdebug
+  ifeq ($(user_variant),userdebug)
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+  else
     ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
   endif
 
