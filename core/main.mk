dde720bbbea0afa2cb17a06415352c6d3c141a56
Samuel Rats <srats@genymobile.com>
2013-11-22 11:20:12 +0100

    [US202] Make userdebug build unsecure (ro.secure=0)

--- core/main.mk
+++ core/main.mk
@@ -287,7 +287,9 @@ enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
-  ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  ifeq ($(user_variant),userdebug)
+	ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=1
+  endif
 
   ifeq ($(user_variant),userdebug)
     # Pick up some extra useful tools
